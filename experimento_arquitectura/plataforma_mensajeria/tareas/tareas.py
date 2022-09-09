33fc7b7cd64abf6d70fa2e12263207fab734498f
Franz Joseph <frogelez@zenware.com.co>
2022-09-08 21:29:43 -0500

    ISSUE :: Ajuste respuesta y consumo del servicio

--- experimento_arquitectura/plataforma_mensajeria/tareas/tareas.py
+++ experimento_arquitectura/plataforma_mensajeria/tareas/tareas.py
@@ -8,8 +8,10 @@ celery_app = Celery('tasks', broker='redis://localhost:6379/0')
 @celery_app.task(name='crear_regla')
 def crear_regla(nombre_regla, descripcion_regla):
     payload = dict(nombre=nombre_regla, descripcion=descripcion_regla)
-    content = requests.post('http://127.0.0.1:5002/reglas', data=payload)
-    if content.status_code == 404:
-            return content.json(), 404
+    content = requests.post('http://127.0.0.1:5002/reglas', json=payload)
+    print(content.json())
+    print(payload)
+    if content.status_code == 200:
+        return {'mensaje':'Regla Creada Exitosamente'}, 200            
     else:
-        return {'mensaje':'Regla Creada Exitosamente'}, 200
+        return content.json(), 500
