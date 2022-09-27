aca55d275c43cdfaac7d6b1b97131e5094bf4411
JHONNATHAN RICARDO ALBARRACIN PERALTA <jhonnathanricardoalbarracinperalta@JHONNATHANs-MacBook-Pro.local>
2022-09-26 21:40:13 -0500

    apigateway - editarusuario

--- experimento_seguridad/api_gateway/app.py
+++ experimento_seguridad/api_gateway/app.py
@@ -4,9 +4,6 @@ from flask_restful import Resource, Api
 from flask import Flask, request
 import requests
 import json
-from celery import Celery
-
-celery_app = Celery(__name__, broker='redis://localhost:6379/0')
 
 app = create_app('default')
 app_contex = app.app_context()
@@ -14,18 +11,15 @@ app_contex.push()
 
 api = Api(app)
 
-@celery_app.task(name='crear_regla')
-def crear_regla(nombre_regla, descripcion_regla, retry):
-    pass
-
 class VistaAdministrarUsuarios(Resource):
 
     def post(self):
-        nombre_regla = request.json['nombre']
-        descripcion_regla = request.json['descripcion']
-        retry = 0
-        args = (nombre_regla, descripcion_regla, retry)
-        crear_regla.apply_async(args)
-        return {'mensaje':'Se edito el usuario correctamente'}, 200
+      
+        content = requests.post('http://127.0.0.1:5000/usuario', json=request.json)
+        print(content.json())      
+        if content.status_code == 200:
+            return content.json(), 200            
+        else:           
+            return content.json(), 500
 
 api.add_resource(VistaAdministrarUsuarios, '/editar_usuario')
